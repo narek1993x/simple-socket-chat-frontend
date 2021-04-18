f6f7da3d2b28a5bf6a98f68d059db28c97662fb8
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2021-04-18 18:15:33 +0400

    Create server for client

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: ['babel-polyfill', './index.js'],
+  entry: ["babel-polyfill", "./index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "build"),
+    publicPath: "",
+    filename: "[name].bundle.js",
   },
-  devtool: '#source-map',
+  devtool: "#source-map",
   module: {
     rules: [
       {
-        use: 'babel-loader',
+        use: "babel-loader",
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        loader: ['style-loader', 'css-loader'],
-        include: __dirname + '/'
+        loader: ["style-loader", "css-loader"],
+        include: __dirname + "/",
       },
       {
         test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader',
-        include: __dirname + '/'
-      }
-    ]
+        loader: "style-loader!css-loader!stylus-loader",
+        include: __dirname + "/",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   devServer: {
     historyApiFallback: true,
     port: 3000,
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 1000
-    }
+      poll: 1000,
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-      inject: 'body'
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+      inject: "body",
     }),
-    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") })
-  ]
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("development") }),
+  ],
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,47 +1,47 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', './index.js'],
+  entry: ["babel-polyfill", "./index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "build"),
+    publicPath: "",
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
-        use: 'babel-loader',
+        use: "babel-loader",
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        loader: ['style-loader', 'css-loader'],
-        include: __dirname + '/'
+        loader: ["style-loader", "css-loader"],
+        include: __dirname + "/",
       },
       {
         test: /\.styl$/,
-        loader: 'style-loader!css-loader!stylus-loader',
-        include: __dirname + '/'
-      }
-    ]
+        loader: "style-loader!css-loader!stylus-loader",
+        include: __dirname + "/",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-      inject: 'body'
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+      inject: "body",
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
     }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 };
